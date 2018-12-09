62eb420f8b66334cdef062c60a7c1e2225f6b614
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-09 02:04:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,8 +229,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const adminprefix = "W";
+const devs = ['461059496861630465'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
